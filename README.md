578ab8560268851d9aec3e645e8830b71c1eddba
KomilovQobiljon <komilov19inha@gmail.com>
2023-09-08 16:07:24 +0900

    first commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# interng4
-Develop an online shop with react, redux, ant design, and others
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import type { FC } from "react";
+
+const App: FC = () => {
+  return <div>Welcome to react</div>;
+};
+
+export default App;
