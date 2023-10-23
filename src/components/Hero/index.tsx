bc82ad540208b2880f7fef2577dadd33146f6c58
KomilovQobiljon <komilov19inha@gmail.com>
2023-10-23 21:58:51 +0900

    site_map

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -22,6 +22,25 @@ const Hero: FC = () => (
             LET'S MAKE BETTER
             <span className="text-[#46A358]">PLANET</span>
           </h1>
+          <p className="text-sm font-normal text-[#727272] w-3/5 max-lg:text-xs mt-[10px] ">
+            We are an online plant shop offering a wide range of cheap and
+            trendy plants. Use our plants to create an unique Urban Jungle.
+            Order your favorite plants!
+          </p>
+          <button className="bg-[#46A358] flex rounded-md items-center justify-center gap-1 text-base text-white mt-[40px] w-[140px] h-9 max-md:mt-[10px]">
+            GET CREDITS
+          </button>
+        </div>
+        <div className="flex-[1] relative flex justify-center items-center max-md:hidden">
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/aema-image-upload.appspot.com/o/greenshop%2Fimages%2Fflower1.png?alt=media&token=0b53d608-7264-4c54-b497-a9bf054fcd9d"
+            alt="hero-image"
+          />
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/aema-image-upload.appspot.com/o/greenshop%2Fimages%2Fflower2.png?alt=media&token=905a94e2-1250-4e56-9dcb-d16bbb1a31ca"
+            className="absolute bottom-0 left-0"
+            alt="hero-image"
+          />
         </div>
       </div>
     </div>
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -3,31 +3,35 @@ import {
   LoginOutlined,
   SearchOutlined,
   BellOutlined,
-  ShoppingCartOutlined
+  ShoppingCartOutlined,
+  MenuOutlined
 } from "@ant-design/icons";
+import SiteMap from "./SiteMap";
 
 const Navbar: FC = () => {
   const ICON_STYLE: string = "cursor-pointer text-[20px]";
   return (
     <div className="p-8 flex align-center border-b border-[#46A358]">
+      <SiteMap />
       <div className="flex-1 justify-start">
         <img
           src="https://firebasestorage.googleapis.com/v0/b/aema-image-upload.appspot.com/o/greenshop%2Ficons%2Flogo.svg?alt=media&token=fc9659d6-f435-43b9-a624-8b0d3a574baa"
           alt="logo"
         />
       </div>
-      <div className="flex-1 justify-center flex gap-4">
+      <div className="flex-1 justify-center flex gap-4 max-md:hidden">
         <h3 className="cursor-pointer">Home</h3>
         <h3 className="cursor-pointer">Blog</h3>
       </div>
-      <div className="flex-1 justify-end flex items-center gap-8 max-sm:hidden">
+      <div className="flex-1 justify-end flex items-center gap-8">
         <SearchOutlined className={ICON_STYLE} />
         <BellOutlined className={ICON_STYLE} />
         <ShoppingCartOutlined className={ICON_STYLE} />
-        <button className="bg-[#46A358] flex rounded-md w-24 items-center justify-center gap-1 h-9 text-base text-white cursor-pointer">
+        <button className="bg-[#46A358] flex rounded-md w-24 items-center justify-center gap-1 h-9 text-base text-white cursor-pointer max-md:hidden">
           <LoginOutlined />
           Login
         </button>
+        <MenuOutlined className={`${ICON_STYLE} hidden max-md:flex`} />
       </div>
     </div>
   );
